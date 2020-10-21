cad58a6f2d4ba3a650428afb1db636d3e0a4c8e1
Nayeem Jahan Rafi <nayeem.jr@samsung.com>
2020-10-21 20:57:37 +0600

    Send message module ready

--- wear/src/main/java/com/example/communicationdemo/MainActivity.kt
+++ wear/src/main/java/com/example/communicationdemo/MainActivity.kt
@@ -2,12 +2,25 @@ package com.example.communicationdemo
 
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.communicationdemo.databinding.ActivityMainBinding
 
 class MainActivity : WearableActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+
+        var hudaiVariable = 0
+        val messagePath = "/message_path"
+
+        binding.button.setOnClickListener{
+            Log.i("WearableMainActivity", "Send Message Button Clicked")
+            hudaiVariable += 1
+            MessageSender.sendMessage(messagePath,"Message from Wearable $hudaiVariable",this)
+        }
 
         // Enables Always-on
         setAmbientEnabled()
