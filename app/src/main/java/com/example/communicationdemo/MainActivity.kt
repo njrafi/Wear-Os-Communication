4405cf91c70dd01529a2cc8dc828ce0ea259e252
Nayeem Jahan Rafi <nayeem.jr@samsung.com>
2020-10-21 21:44:20 +0600

    Message comm wokring

--- app/src/main/java/com/example/communicationdemo/MainActivity.kt
+++ app/src/main/java/com/example/communicationdemo/MainActivity.kt
@@ -7,7 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val gg = MessageListenerService()
     }
 
-    
+
 }
