47888956151b6a97cfe28a8afd207411df15da28
Nayeem Jahan Rafi <nayeem.jr@samsung.com>
2020-10-27 23:37:05 +0600

    Added take photo option

--- app/src/main/java/com/example/communicationdemo/MainActivity.kt
+++ app/src/main/java/com/example/communicationdemo/MainActivity.kt
@@ -4,13 +4,19 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.communicationdemo.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val CameraRequestCode = 0
 
     private val messageBroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
@@ -21,12 +27,28 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main
+        )
 
-        val i = Intent(this, MessageListenerService::class.java)
-        this.startService(i)
+        binding.takePhotoButton.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(takePictureIntent, CameraRequestCode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_CANCELED) return
+        if (requestCode == CameraRequestCode) {
+            if (resultCode == RESULT_OK && data != null) {
+                val bitmapImage = data.extras?.get("data") as Bitmap?
+                imageView.setImageBitmap(bitmapImage)
+            }
+        }
     }
 
     override fun onStart() {
