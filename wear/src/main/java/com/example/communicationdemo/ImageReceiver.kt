a8d707ce9d4278914fefecb7f23a63b090059430
Nayeem Jahan Rafi <nayeem.jr@samsung.com>
2020-10-28 01:04:32 +0600

    Refactor Code

--- wear/src/main/java/com/example/communicationdemo/ImageReceiver.kt
+++ wear/src/main/java/com/example/communicationdemo/ImageReceiver.kt
@@ -21,13 +21,14 @@ object ImageReceiver {
     val backgroundScope = CoroutineScope(Dispatchers.IO + job)
 
     private fun broadcastImagePathUri(imagePathUri: String?, context: Context) {
-        if(imagePathUri == null) return
+        if (imagePathUri == null) return
         val intent = Intent()
         intent.action = ImageMessageConstants.intentName
         intent.putExtra(ImageMessageConstants.imagePathUri, imagePathUri)
         Log.i(tag, "Image Broadcasts")
         LocalBroadcastManager.getInstance(context).sendBroadcast(intent)
     }
+
     fun registerChannel(context: Context) {
         Wearable.getChannelClient(context)
             .registerChannelCallback(object : ChannelClient.ChannelCallback() {
@@ -40,21 +41,20 @@ object ImageReceiver {
                         val fileUri = Uri.fromFile(outFile)
                         Wearable.getChannelClient(context)
                             .receiveFile(channel, fileUri, false)
-                        Wearable.getChannelClient(context)
-                            .registerChannelCallback(object : ChannelClient.ChannelCallback() {
-                                override fun onInputClosed(
-                                    channel: ChannelClient.Channel,
-                                    i: Int,
-                                    i1: Int
-                                ) {
-                                    super.onInputClosed(channel, i, i1)
-                                    Log.d(tag, "onInputClosed")
-
-                                    broadcastImagePathUri(fileUri.path, context)
-                                    Wearable.getChannelClient(context).close(channel)
-                                }
-                            })
+                        registerInputClosedCallback(context, fileUri)
                     }
+                }
+            })
+    }
+
+    private fun registerInputClosedCallback(context: Context, fileUri: Uri) {
+        Wearable.getChannelClient(context)
+            .registerChannelCallback(object : ChannelClient.ChannelCallback() {
+                override fun onInputClosed(channel: ChannelClient.Channel, i: Int, i1: Int) {
+                    super.onInputClosed(channel, i, i1)
+                    Log.d(tag, "onInputClosed")
+                    broadcastImagePathUri(fileUri.path, context)
+                    Wearable.getChannelClient(context).close(channel)
                 }
             })
     }
